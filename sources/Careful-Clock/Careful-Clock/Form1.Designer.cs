dde2b62b7dcf2fe667b4d1b76de65f1d00e0c7b1
PMH Studio / CS <hj666c3@gmail.com>
2019-05-21 20:26:23 +0900

    complete modifing schedule funtion

--- sources/Careful-Clock/Careful-Clock/Form1.Designer.cs
+++ sources/Careful-Clock/Careful-Clock/Form1.Designer.cs
@@ -46,7 +46,16 @@
             this.textBox14 = new System.Windows.Forms.TextBox();
             this.textBox15 = new System.Windows.Forms.TextBox();
             this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -55,134 +64,251 @@
             // 
             // textBox1
             // 
-            this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(41, 33);
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(147, 33);
+            this.textBox2.Location = new System.Drawing.Point(204, 10);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(41, 60);
+            this.textBox3.Location = new System.Drawing.Point(12, 50);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 21);
             this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(147, 60);
+            this.textBox4.Location = new System.Drawing.Point(204, 50);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 21);
             this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(41, 87);
+            this.textBox5.Location = new System.Drawing.Point(12, 90);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 21);
             this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(147, 87);
+            this.textBox6.Location = new System.Drawing.Point(204, 90);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 21);
             this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.TextBox6_TextChanged);
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(41, 114);
+            this.textBox7.Location = new System.Drawing.Point(12, 130);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 21);
             this.textBox7.TabIndex = 6;
+            this.textBox7.TextChanged += new System.EventHandler(this.TextBox7_TextChanged);
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(147, 114);
+            this.textBox8.Location = new System.Drawing.Point(204, 130);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 21);
             this.textBox8.TabIndex = 7;
+            this.textBox8.TextChanged += new System.EventHandler(this.TextBox8_TextChanged);
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(41, 141);
+            this.textBox9.Location = new System.Drawing.Point(12, 170);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(100, 21);
             this.textBox9.TabIndex = 8;
+            this.textBox9.TextChanged += new System.EventHandler(this.TextBox9_TextChanged);
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(147, 141);
+            this.textBox10.Location = new System.Drawing.Point(204, 210);
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(100, 21);
             this.textBox10.TabIndex = 9;
+            this.textBox10.TextChanged += new System.EventHandler(this.TextBox10_TextChanged);
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(41, 168);
+            this.textBox11.Location = new System.Drawing.Point(12, 250);
             this.textBox11.Name = "textBox11";
             this.textBox11.Size = new System.Drawing.Size(100, 21);
             this.textBox11.TabIndex = 10;
+            this.textBox11.TextChanged += new System.EventHandler(this.TextBox11_TextChanged);
             // 
             // textBox12
             // 
-            this.textBox12.Location = new System.Drawing.Point(147, 168);
+            this.textBox12.Location = new System.Drawing.Point(204, 170);
             this.textBox12.Name = "textBox12";
             this.textBox12.Size = new System.Drawing.Size(100, 21);
             this.textBox12.TabIndex = 11;
+            this.textBox12.TextChanged += new System.EventHandler(this.TextBox12_TextChanged);
             // 
             // textBox13
             // 
-            this.textBox13.Location = new System.Drawing.Point(41, 195);
+            this.textBox13.Location = new System.Drawing.Point(12, 210);
             this.textBox13.Name = "textBox13";
             this.textBox13.Size = new System.Drawing.Size(100, 21);
             this.textBox13.TabIndex = 12;
+            this.textBox13.TextChanged += new System.EventHandler(this.TextBox13_TextChanged);
             // 
             // textBox14
             // 
-            this.textBox14.Location = new System.Drawing.Point(147, 196);
+            this.textBox14.Location = new System.Drawing.Point(204, 250);
             this.textBox14.Name = "textBox14";
             this.textBox14.Size = new System.Drawing.Size(100, 21);
             this.textBox14.TabIndex = 13;
+            this.textBox14.TextChanged += new System.EventHandler(this.TextBox14_TextChanged);
             // 
             // textBox15
             // 
-            this.textBox15.Location = new System.Drawing.Point(41, 223);
+            this.textBox15.Location = new System.Drawing.Point(12, 290);
             this.textBox15.Name = "textBox15";
             this.textBox15.Size = new System.Drawing.Size(100, 21);
             this.textBox15.TabIndex = 14;
+            this.textBox15.TextChanged += new System.EventHandler(this.TextBox15_TextChanged);
             // 
             // textBox16
             // 
-            this.textBox16.Location = new System.Drawing.Point(147, 223);
+            this.textBox16.Location = new System.Drawing.Point(204, 290);
             this.textBox16.Name = "textBox16";
             this.textBox16.Size = new System.Drawing.Size(100, 21);
             this.textBox16.TabIndex = 15;
+            this.textBox16.TextChanged += new System.EventHandler(this.TextBox16_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 13F);
+            this.label1.Location = new System.Drawing.Point(118, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "아침자습";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 13F);
+            this.label2.Location = new System.Drawing.Point(130, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "1교시";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 13F);
+            this.label3.Location = new System.Drawing.Point(130, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "2교시";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 13F);
+            this.label4.Location = new System.Drawing.Point(130, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "3교시";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 13F);
+            this.label5.Location = new System.Drawing.Point(130, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 18);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "4교시";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 13F);
+            this.label6.Location = new System.Drawing.Point(130, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 18);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "5교시";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 13F);
+            this.label7.Location = new System.Drawing.Point(130, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 18);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "6교시";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("굴림", 13F);
+            this.label8.Location = new System.Drawing.Point(130, 293);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 18);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "7교시";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 250);
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("굴림", 19F);
+            this.button1.Location = new System.Drawing.Point(12, 325);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(206, 44);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "수정하기";
+            this.button1.Size = new System.Drawing.Size(135, 44);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "적용";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 19F);
+            this.button2.Location = new System.Drawing.Point(167, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 44);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "새로고침";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(294, 333);
+            this.ClientSize = new System.Drawing.Size(314, 381);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox16);
             this.Controls.Add(this.textBox15);
             this.Controls.Add(this.textBox14);
@@ -200,7 +326,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Careful Clock (시간관리자)";
+            this.Text = "Careful Clock";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -226,7 +352,16 @@
         private System.Windows.Forms.TextBox textBox14;
         private System.Windows.Forms.TextBox textBox15;
         private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- sources/Careful-Clock/Careful-Clock/Form1.cs
+++ sources/Careful-Clock/Careful-Clock/Form1.cs
@@ -13,6 +13,7 @@ namespace Careful_Clock
     //https://api.myjson.com/bins/1b2lp2
     public partial class Form1 : Form
     {
+        private JObject json = new JObject();
         public Form1()
         {
             InitializeComponent();
@@ -25,11 +26,10 @@ namespace Careful_Clock
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            WebClient cl = new WebClient();
-            cl.Encoding = Encoding.UTF8;
-            string a = cl.DownloadString("https://api.myjson.com/bins/1b2lp2");
-            textBox1.Text = a;
-            JObject json = JObject.Parse(a);
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            string read = client.DownloadString("https://api.myjson.com/bins/1b2lp2");
+            json = JObject.Parse(read);
             textBox1.Text = json["0"]["시작"].ToString();
             textBox2.Text = json["0"]["끝"].ToString();
             textBox3.Text = json["1"]["시작"].ToString();
@@ -46,11 +46,138 @@ namespace Careful_Clock
             textBox14.Text = json["6"]["끝"].ToString();
             textBox15.Text = json["7"]["시작"].ToString();
             textBox16.Text = json["7"]["끝"].ToString();
+            button1.Enabled = false;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox7_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox8_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox9_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox12_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox13_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox10_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox11_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox14_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox15_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void TextBox16_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            json["0"]["시작"] = textBox1.Text;
+            json["0"]["끝"] = textBox2.Text;
+            json["1"]["시작"] = textBox3.Text;
+            json["1"]["끝"] = textBox4.Text;
+            json["2"]["시작"] = textBox5.Text;
+            json["2"]["끝"] = textBox6.Text;
+            json["3"]["시작"] = textBox7.Text;
+            json["3"]["끝"] = textBox8.Text;
+            json["4"]["시작"] = textBox9.Text;
+            json["4"]["끝"] = textBox10.Text;
+            json["5"]["시작"] = textBox11.Text;
+            json["5"]["끝"] = textBox12.Text;
+            json["6"]["시작"] = textBox13.Text;
+            json["6"]["끝"] = textBox14.Text;
+            json["7"]["시작"] = textBox15.Text;
+            json["7"]["끝"] = textBox16.Text;
+            WebClient upload = new WebClient();
+            upload.Encoding = Encoding.UTF8;
+            upload.Headers.Add("Content-Type","application/json");
+            upload.UploadString("https://api.myjson.com/bins/1b2lp2", "Put",json.ToString());
+            button1.Enabled = false;
+            MessageBox.Show("업로드가 완료되었습니다.","완료");
+        }
 
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            string read = client.DownloadString("https://api.myjson.com/bins/1b2lp2");
+            json = JObject.Parse(read);
+            textBox1.Text = json["0"]["시작"].ToString();
+            textBox2.Text = json["0"]["끝"].ToString();
+            textBox3.Text = json["1"]["시작"].ToString();
+            textBox4.Text = json["1"]["끝"].ToString();
+            textBox5.Text = json["2"]["시작"].ToString();
+            textBox6.Text = json["2"]["끝"].ToString();
+            textBox7.Text = json["3"]["시작"].ToString();
+            textBox8.Text = json["3"]["끝"].ToString();
+            textBox9.Text = json["4"]["시작"].ToString();
+            textBox10.Text = json["4"]["끝"].ToString();
+            textBox11.Text = json["5"]["시작"].ToString();
+            textBox12.Text = json["5"]["끝"].ToString();
+            textBox13.Text = json["6"]["시작"].ToString();
+            textBox14.Text = json["6"]["끝"].ToString();
+            textBox15.Text = json["7"]["시작"].ToString();
+            textBox16.Text = json["7"]["끝"].ToString();
+            button1.Enabled = false;
         }
     }
 }
