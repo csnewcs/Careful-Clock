653dfe5c386f57d82a2a68bc181ecbd15cfc3fba
PMH Studio / CS <hj666c3@gmail.com>
2019-06-04 13:25:11 +0900

    BACKUP

---
+++ sources/Careful-Clock/Edit-Schedule/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Edit_Schedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("굴림", 20F);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(589, 304);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "JSON파일|*.json|메모장 파일|*.txt";
+            this.openFileDialog1.Title = "json열기";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "업로드";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "다운로드";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(453, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 60);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "파일 열기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(319, 322);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 60);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "파일 저장";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 394);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ sources/Careful-Clock/Edit-Schedule/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edit_Schedule
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+            richTextBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            client.Headers.Add("Content-Type", "application/json");
+            client.UploadString("https://api.myjson.com/bins/1b2lp2", "Put", richTextBox1.Text);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            richTextBox1.Text = client.DownloadString("https://api.myjson.com/bins/1b2lp2");
+        }
+    }
+}
